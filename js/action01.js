edd1fb5c972366c9fd34d5ec79f53355fa08a69b
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-09-21 23:58:56 -0300

    treating dimensions in tensorflow

---
+++ js/action01.js
@@ -0,0 +1,50 @@
+﻿document.getElementById('result').textContent = '';
+
+const executar = () => {
+	let txt = '';
+
+	// tensor escalar
+	const escalar = tf.scalar(1.5);
+
+	// tensor unidimensional
+	const tensor1d = tf.tensor1d([1, 2, 3]);
+
+	// tensor multidimensional
+	/* 
+		Abaixo estamos implementando um tensor bidimensional formado por duas linhas
+		e duas colunas. 
+		|1 , 2|
+		|3 , 4|
+
+		Podemos escrever um tensor bidimensional da seguinte forma:
+		const tensor2d = tf.tensor2d([[1,2], [3,4]]);
+
+		Também temos um tensor tridimensional
+		Podemos escrever um tensor tridimensional da seguinte forma:
+		Os elementos 1,2,3,4 sendo 2 linhas 2 colunas e elementos unicos
+		const tensor2d = tf.tensor3d([1,2,3,4], [2,2,1]);
+	*/
+	const tensor2d = tf.tensor2d([1, 2, 3, 4], [2, 2]);
+	const tensor3d = tf.tensor3d([[[1], [2]], [[3], [4]]]);
+
+	txt = `
+		Tensor escalar:
+		${escalar.toString()}
+
+		Tensor Unidimensional:
+		${tensor1d.toString()}
+
+		Tensor Multidimensional:
+		Duas dimensões
+		${tensor2d.toString()}
+
+		Três dimensões
+		${tensor3d.toString()}
+	`;
+
+	exibir(txt);
+}
+
+const exibir = (str = '') => {
+	document.getElementById('result').textContent = str;
+}
